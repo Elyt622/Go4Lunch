7721af14f838c4b02cf02fdd6e77d678d4dfd2e5
yohan <eras.projet@gmail.com>
2020-06-16 18:46:08 +0200

    Update auth with email on Firebase

--- app/src/main/java/com/elytevolution/go4lunch/MainActivity.java
+++ app/src/main/java/com/elytevolution/go4lunch/MainActivity.java
@@ -3,15 +3,58 @@ package com.elytevolution.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.main_activity_button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLoginButton();
+            }
+        });
+    }
+
+    //FOR DATA
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    public void onClickLoginButton() {
+        // 3 - Launch Sign-In Activity when user clicked on Login Button
+        this.startSignInActivity();
+    }
+
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
