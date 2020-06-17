f889f475c80f356af9973e8916e386fd4c615b03
yohan <eras.projet@gmail.com>
2020-06-17 05:53:17 +0200

    Add Facebook and Google authentication

--- app/src/main/java/com/elytevolution/go4lunch/MainActivity.java
+++ app/src/main/java/com/elytevolution/go4lunch/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 
@@ -52,7 +54,9 @@ public class MainActivity extends AppCompatActivity {
                         .createSignInIntentBuilder()
                         .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(
-                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build()))
                         .setIsSmartLockEnabled(false, true)
                         .build(),
                 RC_SIGN_IN);
