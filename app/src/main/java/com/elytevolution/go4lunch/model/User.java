b3eb48a8e8ca87882c096a4857c4c1dc6be63337
yohan <eras.projet@gmail.com>
2020-10-31 06:23:22 +0100

    Update User Class for Firestore implementation

--- app/src/main/java/com/elytevolution/go4lunch/model/User.java
+++ app/src/main/java/com/elytevolution/go4lunch/model/User.java
@@ -1,33 +1,35 @@
 package com.elytevolution.go4lunch.model;
 
+import androidx.annotation.Nullable;
+
 public class User {
 
     private String uid;
 
-    private String name;
+    private String firstName;
 
-    private String surname;
+    private String lastName;
 
     private String email;
 
-    private String urlPicture;
+    @Nullable private String urlPicture;
 
-    public User(String uid, String name, String surname, String email, String urlPicture) {
+    public User(String uid, String firstName, String lastName, String email, @Nullable String urlPicture) {
         this.uid = uid;
-        this.name = name;
-        this.surname = surname;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.urlPicture = urlPicture;
     }
 
     public String getId() { return uid; }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getSurname() {
-        return surname;
+    public String getLastName() {
+        return lastName;
     }
 
     public String getEmail() {
