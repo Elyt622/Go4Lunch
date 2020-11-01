298a09f80e88240bc34e88da5f8699175a173cca
yohan <eras.projet@gmail.com>
2020-11-01 04:19:12 +0100

    Add facebook authentication

--- app/src/main/java/com/elytevolution/go4lunch/view/activity/LoginActivity.java
+++ app/src/main/java/com/elytevolution/go4lunch/view/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,8 +13,18 @@ import android.view.View;
 import com.elytevolution.go4lunch.R;
 import com.elytevolution.go4lunch.databinding.ActivityLoginBinding;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.LoginStatusCallback;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import com.firebase.ui.auth.data.remote.FacebookSignInHandler;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -24,17 +35,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
 
     private static final int RC_SIGN_IN = 123;
-
-    private ConstraintLayout constraintLayout;
 
     private LoginButton facebookLoginButton;
 
@@ -44,42 +56,62 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
 
+    private FirebaseUser firebaseUser;
+
+    private GoogleSignInOptions gso;
+
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        constraintLayout = binding.activityLoginConstraintLayout;
+
         facebookLoginButton = binding.loginButton;
         googleLoginButton = binding.signInButton;
+
         auth = FirebaseAuth.getInstance();
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+        googleLoginButton.setOnClickListener(v -> configureAuthGoogle());
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        facebookLoginButton.setOnClickListener(v -> configureAuthFacebook(this));
+     }
 
-        googleLoginButton.setSize(SignInButton.SIZE_STANDARD);
+    private void configureAuthFacebook(Context context) {
+        callbackManager = CallbackManager.Factory.create();
+        facebookLoginButton.setPermissions("email", "public_profile");
+        facebookLoginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        firebaseAuthWithFacebook(loginResult.getAccessToken().getToken());
+                    }
 
-        googleLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signInWithGoogle();
-            }
-        });
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
 
-        facebookLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });    }
 
-            }
-        });
+
+    private void configureAuthGoogle(){
+         // Configure sign-in to request the user's ID, email address, and basic
+         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                 .requestIdToken(getString(R.string.default_web_client_id))
+                 .requestEmail()
+                 .build();
+         // Build a GoogleSignInClient with the options specified by gso.
+         googleSignInClient = GoogleSignIn.getClient(this, gso);
+         googleLoginButton.setSize(SignInButton.SIZE_STANDARD);
+         signInWithGoogle();
      }
 
     private void signInWithGoogle() {
@@ -107,9 +139,30 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
+    private void firebaseAuthWithFacebook(String idToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(idToken);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
