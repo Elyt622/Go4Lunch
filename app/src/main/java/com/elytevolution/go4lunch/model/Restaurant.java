e7167b473ae9b7313057c58198d9eae7db3651ed
yohan <eras.projet@gmail.com>
2020-11-04 10:34:06 +0100

    Add Restaurant class, RestaurantHelper class and add all restaurants IDs in Firestore

---
+++ app/src/main/java/com/elytevolution/go4lunch/model/Restaurant.java
@@ -0,0 +1,14 @@
+package com.elytevolution.go4lunch.model;
+
+public class Restaurant {
+
+    String idPlace;
+
+    public Restaurant(String idPlace){
+        this.idPlace = idPlace;
+    }
+
+    public String getIdPlace() {
+        return idPlace;
+    }
+}
--- app/src/main/java/com/elytevolution/go4lunch/view/fragment/ListFragment.java
+++ app/src/main/java/com/elytevolution/go4lunch/view/fragment/ListFragment.java
@@ -2,6 +2,7 @@ package com.elytevolution.go4lunch.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,18 +16,27 @@ import android.view.ViewGroup;
 
 import com.elytevolution.go4lunch.R;
 import com.elytevolution.go4lunch.model.NearBySearch;
+import com.elytevolution.go4lunch.model.Restaurant;
 import com.elytevolution.go4lunch.utilis.GooglePlaceCalls;
 import com.elytevolution.go4lunch.view.adapter.RestaurantListAdapter;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.elytevolution.go4lunch.RestaurantHelper.createRestaurant;
+import static com.elytevolution.go4lunch.RestaurantHelper.getRestaurant;
+import static com.elytevolution.go4lunch.RestaurantHelper.getRestaurantCollection;
+import static com.elytevolution.go4lunch.UserHelper.createUser;
 
 public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks{
 
     private RecyclerView recyclerView;
 
-    private final List<NearBySearch.Results> tests = new ArrayList<>();
+    private final List<NearBySearch.Results> results = new ArrayList<>();
 
     private RestaurantListAdapter adapter;
 
@@ -35,6 +45,8 @@ public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private LatLng location;
+
+    private List<Restaurant> restaurants = new ArrayList<>();
 
     public ListFragment(LatLng location) {
         this.location = location;
@@ -51,9 +63,11 @@ public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks
         view = inflater.inflate(R.layout.fragment_list, container, false);
         swipeRefreshLayout = view.findViewById(R.id.swipe_fragment_list);
         recyclerView = view.findViewById(R.id.recycler_view_fragment_list);
-        adapter = new RestaurantListAdapter(tests, location);
+
+        adapter = new RestaurantListAdapter(results, location);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
         configureSwipeRefreshLayout();
         return view;
     }
@@ -62,6 +76,7 @@ public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks
     public void onResponse(@Nullable NearBySearch tests) {
         if (tests != null) {
             updateUI(tests.getResults());
+            insertRestaurantInFireStore();
         } else {
             Log.d("DEBUG", "RESPONSE IS NULL");
         }
@@ -69,7 +84,7 @@ public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks
 
     @Override
     public void onFailure() {
-        Log.d("DEBUG", "ECHEC");
+        Log.d("DEBUG", "FAILURE");
     }
 
     // ------------------------------
@@ -96,9 +111,30 @@ public class ListFragment extends Fragment implements GooglePlaceCalls.Callbacks
     }
 
     private void updateUI(List<NearBySearch.Results> results) {
-        this.tests.clear();
-        this.tests.addAll(results);
+        this.results.clear();
+        this.results.addAll(results);
         adapter.notifyDataSetChanged();
         swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void allRestaurant(List<NearBySearch.Results> results){
+        for(NearBySearch.Results result: results){
+            restaurants.add(new Restaurant(result.getPlace_id()));
+        }
+    }
+
+    private void insertRestaurantInFireStore(){
+        allRestaurant(results);
+        for(Restaurant restaurant : restaurants)
+            getRestaurantCollection().document(restaurant.getIdPlace()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        if (!task.getResult().exists()) {
+                            createRestaurant(restaurant.getIdPlace());
+                        }
+                    }
+                }
+            });
     }
 }
