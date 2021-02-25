4c18144c344f0f4d284f2d460420bb73a57a5420
yohan <eras.projet@gmail.com>
2021-02-25 05:15:27 +0100

    Updated SplashActivity, MVC -> MVP

--- app/src/main/java/com/elytevolution/go4lunch/presenter/SplashPresenter.java
+++ app/src/main/java/com/elytevolution/go4lunch/presenter/SplashPresenter.java
@@ -1,5 +1,11 @@
 package com.elytevolution.go4lunch.presenter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+
+import com.elytevolution.go4lunch.view.activity.LoginActivity;
+import com.elytevolution.go4lunch.view.activity.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -7,19 +13,44 @@ public class SplashPresenter {
 
     private SplashPresenter.View view;
 
+    private FirebaseUser currentUser;
+
     public SplashPresenter(SplashPresenter.View view) {
         this.view = view;
+    }
+
+    public void configureSplashScreen(Activity activity){
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent;
+            if(currentUser != null) {
+                intent = new Intent(activity, MainActivity.class);
+            }else {
+                intent = new Intent(activity, LoginActivity.class);
+            }
+            view.navigateToMainActivity(intent);
+        }, 2000);
+    }
+
+    public void initCurrentUser(){
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    public void onStart(Activity activity){
+        initCurrentUser();
+        configureSplashScreen(activity);
+    }
+
+    public void onCreate(Activity activity){
+        initCurrentUser();
+        configureSplashScreen(activity);
     }
 
     public void onDestroy() {
         view = null;
     }
 
-    public FirebaseUser getCurrentUser(){
-        return FirebaseAuth.getInstance().getCurrentUser();
-    }
-
     public interface View {
-
+        void navigateToMainActivity(Intent intent);
     }
 }
--- app/src/main/java/com/elytevolution/go4lunch/view/activity/SplashActivity.java
+++ app/src/main/java/com/elytevolution/go4lunch/view/activity/SplashActivity.java
@@ -2,17 +2,13 @@ package com.elytevolution.go4lunch.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.elytevolution.go4lunch.R;
 import com.elytevolution.go4lunch.presenter.SplashPresenter;
-import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity implements SplashPresenter.View {
-
-    private FirebaseUser currentUser;
 
     private SplashPresenter presenter;
 
@@ -22,26 +18,19 @@ public class SplashActivity extends AppCompatActivity implements SplashPresenter
         setContentView(R.layout.activity_splash);
 
         presenter = new SplashPresenter(this);
+        presenter.onCreate(this);
+    }
 
-        currentUser = presenter.getCurrentUser();
-
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            Intent intent;
-            if(currentUser != null) {
-                intent = new Intent(getBaseContext(), MainActivity.class);
-            }else {
-                intent = new Intent(getBaseContext(), LoginActivity.class);
-            }
-            startActivity(intent);
-            finish();
-        }, 2000);
+    @Override
+    public void navigateToMainActivity(Intent intent) {
+        startActivity(intent);
+        finish();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        currentUser = presenter.getCurrentUser();
+        presenter.onStart(this);
     }
 
     @Override
