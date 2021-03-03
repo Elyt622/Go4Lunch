09d2c8a33a330e37be1f6c9cd9a11099cc434a84
yohan <eras.projet@gmail.com>
2021-03-03 06:25:28 +0100

    Updated MainActivity MVC -> MVP

--- app/src/main/java/com/elytevolution/go4lunch/presenter/MainPresenter.java
+++ app/src/main/java/com/elytevolution/go4lunch/presenter/MainPresenter.java
@@ -1,14 +1,249 @@
 package com.elytevolution.go4lunch.presenter;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.elytevolution.go4lunch.R;
+import com.elytevolution.go4lunch.view.activity.DetailsActivity;
+import com.elytevolution.go4lunch.view.adapter.ViewPagerAdapter;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static com.elytevolution.go4lunch.api.UserHelper.getUser;
+
 public class MainPresenter {
 
-    private final MainPresenter.View view;
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 123;
 
-    public MainPresenter(MainPresenter.View view) {
+    private static final String TAG = "MainPresenter";
+
+    private MainPresenter.View view;
+
+    private LatLng currentLocation = null;
+
+    private double latitude, longitude;
+
+    private final String key;
+
+    private FirebaseUser currentUser;
+
+    private Activity activity;
+
+    public MainPresenter(MainPresenter.View view, String key, Activity activity) {
         this.view = view;
+        this.key = key;
+        this.activity = activity;
     }
 
-    public interface View {
+    private boolean localizationIsEnable(){
+        return ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
 
+    public void startLocalization() {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            if (localizationIsEnable()) {
+                @SuppressLint("MissingPermission") Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (lastKnownLocation != null) {
+                    latitude = lastKnownLocation.getLatitude();
+                }
+                if (lastKnownLocation != null) {
+                    longitude = lastKnownLocation.getLongitude();
+                }
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            }
+        } else {
+            Log.d(TAG, "PERMISSION NOT GRANTED");
+        }
+        currentLocation = new LatLng(latitude, longitude);
+    }
+
+    public void configureAutocompleteSearch() {
+        if (!Places.isInitialized()) {
+            Places.initialize(activity, key, Locale.FRANCE);
+        }
+
+        RectangularBounds bounds = RectangularBounds.newInstance(
+                new LatLng(latitude - 0.007, longitude - 0.007),
+                new LatLng(latitude + 0.007, longitude + 0.007));
+
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.TYPES);
+        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                .setLocationRestriction(bounds)
+                .setCountries(Collections.singletonList("FR")).setTypeFilter(TypeFilter.ESTABLISHMENT)
+                .build(activity);
+        activity.startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    public ViewPager configureViewPager(ViewPager viewPager, FragmentManager fragmentManager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(currentLocation, fragmentManager);
+        viewPager.setAdapter(adapter);
+        return viewPager;
+    }
+
+    private void configureTabLayout(TabLayout tabLayout, ViewPager viewPager, FragmentManager fragmentManager){
+
+        List<String> listNameViewPager = new ArrayList<>();
+        List<Integer> listImageViewPager = new ArrayList<>();
+
+        listNameViewPager.add("Map View");
+        listNameViewPager.add("List View");
+        listNameViewPager.add("Workmates");
+        listImageViewPager.add(R.drawable.baseline_map_black_18dp);
+        listImageViewPager.add(R.drawable.baseline_list_black_18dp);
+        listImageViewPager.add(R.drawable.baseline_group_black_18dp);
+
+        tabLayout.setupWithViewPager(configureViewPager(viewPager, fragmentManager));
+        for (int icon : listImageViewPager) {
+            tabLayout.getTabAt(listImageViewPager.indexOf(icon)).setIcon(icon);
+        }
+        for (String list : listNameViewPager)
+            tabLayout.getTabAt(listNameViewPager.indexOf(list)).setText(list);
+    }
+
+    public void configureActivity(TabLayout tabLayout, ViewPager viewpager, FragmentManager fragmentManager) {
+        if (localizationIsEnable()) {
+            view.setVisibilityLocalizationText(0);
+            view.setVisibilityButtonActivateLocalization(0);
+            configureTabLayout(tabLayout, viewpager, fragmentManager);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE && data != null) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                if(place.getTypes().contains(Place.Type.RESTAURANT)) {
+                    Log.i(TAG, "Place: " + place.getName() + ", " + place.getId() + place.getTypes());
+                    Intent intent = new Intent(activity, DetailsActivity.class);
+                    intent.putExtra("ID", place.getId());
+                    activity.startActivity(intent);
+                }
+                else{
+                    Toast.makeText(activity, "It's not a restaurant", Toast.LENGTH_LONG).show();
+                }
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.i(TAG, "The user canceled the operation");
+            }
+        }
+    }
+
+    public void initUser() {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private boolean isLogged() {
+        return FirebaseAuth.getInstance().getCurrentUser() != null;
+    }
+
+    public void userIsLogged(){
+        if (!isLogged()) {
+            activity.finish();
+        } else {
+            getInfoUserOnMenuHeader();
+        }
+    }
+
+    public void getInfoUserOnMenuHeader() {
+        view.setUserName(currentUser.getDisplayName());
+        view.setEmail(currentUser.getEmail());
+        view.setProfilePicture(currentUser.getPhotoUrl());
+    }
+
+    public void setPermissions(int requestCode, int[] grantResults, TabLayout tabLayout, ViewPager viewpager, FragmentManager fragmentManager) {
+        if (requestCode == 100 && (grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            startLocalization();
+            configureTabLayout(tabLayout, viewpager, fragmentManager);
+            view.setVisibilityButtonActivateLocalization(0);
+            view.setVisibilityLocalizationText(0);
+        }
+    }
+
+    // Action click
+
+    public void clickOnMyLunch(){
+        getUser(currentUser.getUid()).addOnSuccessListener(document -> {
+            String idPlace = document.getString("idPlace");
+            if(idPlace != null && !idPlace.equals("")) {
+                Intent intent = new Intent(activity, DetailsActivity.class);
+                intent.putExtra("ID", idPlace);
+                activity.startActivity(intent);
+            }
+            else {
+                Toast.makeText(activity, "You didn't select a restaurant yet!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void clickOnSettings() {
+        Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+        activity.startActivity(intent);
+    }
+
+    public void clickOnLogout() {
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+        activity.finish();
+    }
+
+    // Activity Cycle
+
+    public void onCreate(TabLayout tabLayout, ViewPager viewpager, FragmentManager fragmentManager) {
+        initUser();
+        userIsLogged();
+        startLocalization();
+        configureActivity(tabLayout, viewpager, fragmentManager);
+    }
+
+    public void onDestroy() {
+        view = null;
+        activity = null;
+    }
+
+    // View
+
+    public interface View {
+        void setProfilePicture(Uri urlPicture);
+        void setUserName(String userName);
+        void setEmail(String email);
+        void setVisibilityLocalizationText(int visibility);
+        void setVisibilityButtonActivateLocalization(int visibility);
     }
 }
--- app/src/main/java/com/elytevolution/go4lunch/view/activity/MainActivity.java
+++ app/src/main/java/com/elytevolution/go4lunch/view/activity/MainActivity.java
@@ -1,66 +1,35 @@
 package com.elytevolution.go4lunch.view.activity;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.elytevolution.go4lunch.R;
 import com.elytevolution.go4lunch.databinding.ActivityMainBinding;
 import com.elytevolution.go4lunch.presenter.MainPresenter;
-import com.elytevolution.go4lunch.view.adapter.ViewPagerAdapter;
-import com.facebook.login.LoginManager;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.RectangularBounds;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteActivity;
-import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
-import static com.elytevolution.go4lunch.api.UserHelper.getUser;
-
 public class MainActivity extends AppCompatActivity implements MainPresenter.View {
 
     private static final String TAG = "MainActivity";
-
-    private static final int AUTOCOMPLETE_REQUEST_CODE = 123;
 
     private ActivityMainBinding binding;
 
@@ -68,19 +37,17 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
 
     private ImageView imageUser;
 
-    private TextView nameUser, emailUser;
-
-    private FirebaseUser currentUser;
-
-    private LatLng currentLocation = null;
+    private TextView nameUser, emailUser, textLocalization;
 
     private Button activateLocalizationButton;
 
-    private TextView textLocalization;
-
-    private double latitude, longitude;
-
     private MainPresenter presenter;
+
+    private ViewPager viewPager;
+
+    private TabLayout tabLayout;
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,43 +56,22 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         View view = binding.getRoot();
         setContentView(view);
 
-        presenter = new MainPresenter(this);
+        presenter = new MainPresenter(this, getString(R.string.google_maps_key), this);
+
+        configureNavigationView();
 
         activateLocalizationButton = findViewById(R.id.button_activate_location_main_activity);
         textLocalization = findViewById(R.id.textview_activate_location_main_activity);
-
-        startLocalization();
-
-        if(!localizationIsEnable()){
-            enableLocalization();
-        }
-        else {
-            textLocalization.setVisibility(View.INVISIBLE);
-            activateLocalizationButton.setVisibility(View.INVISIBLE);
-            configureTabLayout();
-        }
-
-        configureNavigationView();
+        viewPager = binding.mainActivityViewPager;
+        tabLayout = binding.tabLayout;
         drawer = findViewById(R.id.activity_main_drawer_layout);
+        navigationView = findViewById(R.id.activity_main_nav_view);
+
+        presenter.onCreate(tabLayout, viewPager, getSupportFragmentManager());
+
+        activateLocalizationButton.setOnClickListener(v -> presenter.startLocalization());
 
         configureToolbar();
-
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (!isLogged()) {
-            finish();
-        } else {
-            getInfoUserOnMenuHeader();
-        }
-    }
-
-    private void enableLocalization(){
-            activateLocalizationButton.setOnClickListener(v -> startLocalization());
-    }
-
-    public void getInfoUserOnMenuHeader() {
-        nameUser.setText(currentUser.getDisplayName());
-        emailUser.setText(currentUser.getEmail());
-        Glide.with(imageUser).load(currentUser.getPhotoUrl()).apply(RequestOptions.circleCropTransform()).into(imageUser);
     }
 
     @Override
@@ -136,88 +82,37 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
                 drawer.openDrawer(GravityCompat.START);
                 return true;
             case (R.id.action_search):
-                configureAutocompleteSearch();
+                presenter.configureAutocompleteSearch();
         }
         return true;
     }
 
-    private boolean localizationIsEnable(){
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void configureAutocompleteSearch() {
-        if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), getString(R.string.google_maps_key), Locale.FRANCE);
-        }
-
-        RectangularBounds bounds = RectangularBounds.newInstance(
-                new LatLng(latitude - 0.007, longitude - 0.007),
-                new LatLng(latitude + 0.007, longitude + 0.007));
-
-        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.TYPES);
-        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
-                .setLocationRestriction(bounds)
-                .setCountries(Collections.singletonList("FR")).setTypeFilter(TypeFilter.ESTABLISHMENT)
-                .build(this);
-        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == AUTOCOMPLETE_REQUEST_CODE && data != null) {
-            if (resultCode == RESULT_OK) {
-                Place place = Autocomplete.getPlaceFromIntent(data);
-                if(place.getTypes().contains(Place.Type.RESTAURANT)) {
-                    Log.i(TAG, "Place: " + place.getName() + ", " + place.getId() + place.getTypes());
-                    Intent intent = new Intent(this, DetailsActivity.class);
-                    intent.putExtra("ID", place.getId());
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "It's not a restaurant", Toast.LENGTH_LONG).show();
-                }
-            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
-                Status status = Autocomplete.getStatusFromIntent(data);
-                Log.i(TAG, status.getStatusMessage());
-            } else if (resultCode == RESULT_CANCELED) {
-                Log.i(TAG, "The user canceled the operation");
-            }
-            return;
-        }
+        presenter.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+
+
     private void configureNavigationView(){
-        NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
+        navigationView = findViewById(R.id.activity_main_nav_view);
         View headerView = navigationView.getHeaderView(0);
         nameUser = headerView.findViewById(R.id.text_view_display_name_header_nav);
         emailUser = headerView.findViewById(R.id.text_view_email_header_nav);
         imageUser = headerView.findViewById(R.id.imageview_user_header_nav);
+
         navigationView.setNavigationItemSelectedListener(item -> {
             int id = item.getItemId();
             switch (id){
                 case (R.id.activity_main_drawer_lunch):
-                    getUser(currentUser.getUid()).addOnSuccessListener(document -> {
-                        String idPlace = document.getString("idPlace");
-                        if(idPlace != null && !idPlace.equals("")) {
-                            Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
-                            intent.putExtra("ID", idPlace);
-                            startActivity(intent);
-                        }
-                        else {
-                            Toast.makeText(getApplicationContext(), "You didn't select a restaurant yet!", Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    presenter.clickOnMyLunch();
                     break;
                 case (R.id.activity_main_drawer_settings):
-                    Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
-                    startActivity(intent);
+                    presenter.clickOnSettings();
                     break;
                 case (R.id.activity_main_drawer_logout):
-                    FirebaseAuth.getInstance().signOut();
-                    LoginManager.getInstance().logOut();
-                    finish();
+                    presenter.clickOnLogout();
                     break;
                 default:
                     break;
@@ -244,67 +139,43 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         return true;
     }
 
-    private boolean isLogged() {
-        return FirebaseAuth.getInstance().getCurrentUser() != null;
-    }
-
-    private void configureTabLayout(){
-        TabLayout tabLayout = binding.tabLayout;
-
-        List<String> listNameViewPager = new ArrayList<>();
-        List<Integer> listImageViewPager = new ArrayList<>();
-
-        listNameViewPager.add("Map View");
-        listNameViewPager.add("List View");
-        listNameViewPager.add("Workmates");
-        listImageViewPager.add(R.drawable.baseline_map_black_18dp);
-        listImageViewPager.add(R.drawable.baseline_list_black_18dp);
-        listImageViewPager.add(R.drawable.baseline_group_black_18dp);
-
-        tabLayout.setupWithViewPager(configureViewPager());
-        for (int icon : listImageViewPager) {
-            tabLayout.getTabAt(listImageViewPager.indexOf(icon)).setIcon(icon);
-        }
-        for (String list : listNameViewPager)
-            tabLayout.getTabAt(listNameViewPager.indexOf(list)).setText(list);
-    }
-
-    private ViewPager configureViewPager() {
-        ViewPager viewPager = binding.mainActivityViewPager;
-        Log.d(TAG, String.valueOf(currentLocation));
-        ViewPagerAdapter adapter = new ViewPagerAdapter(currentLocation, getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        return viewPager;
-    }
-
-    private void startLocalization() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null) {
-            if (localizationIsEnable()) {
-                @SuppressLint("MissingPermission") Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                if (lastKnownLocation != null) {
-                    latitude = lastKnownLocation.getLatitude();
-                }
-                if (lastKnownLocation != null) {
-                    longitude = lastKnownLocation.getLongitude();
-                }
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
-            }
-        } else {
-            Log.d(TAG, "PERMISSION NOT GRANTED");
-        }
-        currentLocation = new LatLng(latitude, longitude);
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 100 && (grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-            startLocalization();
-            configureTabLayout();
+        presenter.setPermissions(requestCode, grantResults, tabLayout, viewPager, getSupportFragmentManager());
+    }
+
+    @Override
+    public void setProfilePicture(Uri urlPicture) {
+        Glide.with(imageUser).load(urlPicture).apply(RequestOptions.circleCropTransform()).into(imageUser);
+    }
+
+    @Override
+    public void setUserName(String userName) {
+        nameUser.setText(userName);
+    }
+
+    @Override
+    public void setEmail(String email) {
+        emailUser.setText(email);
+
+    }
+
+    @Override
+    public void setVisibilityLocalizationText(int visibility) {
+        if(visibility == 0){
             textLocalization.setVisibility(View.INVISIBLE);
+        }else {
+            textLocalization.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void setVisibilityButtonActivateLocalization(int visibility) {
+        if(visibility == 0){
             activateLocalizationButton.setVisibility(View.INVISIBLE);
-         }
+        }else {
+            activateLocalizationButton.setVisibility(View.VISIBLE);
+        }
     }
 }
